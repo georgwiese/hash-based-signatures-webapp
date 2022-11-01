d912347d08c1e272475b8ead3497dbd1a6feb20b
Georg Wiese <georgwiese@gmail.com>
2022-11-01 19:06:45 +0100

    Continue webapp

--- site/index.js
+++ site/index.js
@@ -1,3 +1,9 @@
 import("./node_modules/hash-based-signatures-webapp/hash_based_signatures_webapp.js").then((js) => {
-    js.greet("WebAssembly with npm");
+    function onSubmit() {
+        js.greet("WebAssembly with npm");
+        console.log(document.forms["form"]["public_key"].value);
+        console.log(document.forms["form"]["file"].value);
+        return false;
+    }
+    window.onSubmit = onSubmit;
 });
